4803dd34aa695d289c0f0f17277f0ab2eefe0170
antonkulaga <antonkulaga@gmail.com>
2017-11-27 16:43:41 +0200

    microarrays

---
+++ containers/microarrays/install.R
@@ -0,0 +1,15 @@
+url <- "http://bioconductor.org/packages/3.5/bioc"
+
+if ("BiocInstaller" %in% rownames(installed.packages()))
+remove.packages("BiocInstaller")
+install.packages("BiocInstaller", repos=url)
+
+to_install <- c("Matrix", "KernSmooth", "mgcv", "devtools", "biomaRt", "limma",
+                "affy", "lumi", "methylumiR", "minfi", "stringr", "GEOquery", "GEOmetadb",
+                "ExiMiR", "AgiMicroRna", "doParallel","foreach", "RJSONIO")
+
+for (pack in to_install)
+    if (!suppressWarnings(require(pack, character.only=TRUE)))
+        BiocInstaller::biocLite(pack)
+suppressWarnings(BiocInstaller::biocValid(fix=TRUE, ask=FALSE))
+suppressWarnings(install.packages("optparse"))
